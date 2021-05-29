2b48eee353c437aa337f3d452d8e9baefc49dcf3
Paulo Matos <batalhadematos@gmail.com>
2021-05-29 19:49:09 +0100

    initial commit

---
+++ extra_addons/hello_world/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 AngOdoo
+# AngOdoo - https://www.angodoo.com
+# Paulo Matos - https://github.com/batalhadematos 
+# Email: batalhadematos@gmail.com
+# License: See LICENSE file for full copyright and licensing details.
+
+#from . import models
---
+++ extra_addons/hello_world/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------
+# Copyright 2019 Odoo
+# Copyright 2019 - to date: AngOdoo (http://www.angodoo.com)
+# Paulo Matos - batalhadematos@gmail.com | pm@angodoo.ao
+# License OPL-1 - Software Proprietário)
+#------------------------------------------------------------
+{
+    'name': 'Hello World - AngOdoo eLearning',
+    'category': 'Tools',
+    "author": "AngOdoo, Paulo Matos",
+    "website": "https:/www.angodoo.com",
+    "contributors": ['Paulo Matos <batalhadematos@gmail.com>',],
+    'version': '12.0.1.0.0',
+    'description': """
+
+Sample Hello World for AngOdoo ERP eLearning tutorial
+
+""",
+
+    'depends': [
+                'base',
+                ],
+    
+    'data': [
+	
+           ],
+    'show_in_apps': True,
+    'angodoo_available': False,
+    'installable': True,
+    'auto_install': False,
+    'license': 'OPL-1',
+}
